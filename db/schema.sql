e24c441aa410cc30b9e0a3d8b9c62fe800b57747
Konstantinos Lampridis <boromir674@hotmail.com>
2025-07-27 15:03:35 +0300

    feat: add unit in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,5 +10,6 @@ CREATE TABLE IF NOT EXISTS key_results (
     objective_id INT REFERENCES objectives(id) ON DELETE CASCADE,
     description TEXT NOT NULL,
     progress INT DEFAULT 0,
-    metric VARCHAR(255)
+    metric VARCHAR(255),
+    unit INT CHECK (unit >= 1 AND unit <= 99) DEFAULT 1
 );
