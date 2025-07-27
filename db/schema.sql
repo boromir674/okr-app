012ca0119e63c8094269e631d7b3fe158728a318
Konstantinos Lampridis <boromir674@hotmail.com>
2025-07-27 12:44:14 +0300

    feat: change progress SQL type to INT

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS objectives (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    progress FLOAT DEFAULT 0.0
+    progress INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS key_results (
     id SERIAL PRIMARY KEY,
     objective_id INT REFERENCES objectives(id) ON DELETE CASCADE,
     description TEXT NOT NULL,
-    progress FLOAT DEFAULT 0.0,
+    progress INT DEFAULT 0,
     metric VARCHAR(255)
 );
